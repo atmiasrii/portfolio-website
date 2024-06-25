39156636d687916779c9e50cdbb7c5aa070cc310
atmiasrii <atmiasrii@gmail.com>
2024-06-25 10:34:54 -0700

    Updated Webpack configuration

--- bundler/webpack.prod.js
+++ bundler/webpack.prod.js
@@ -2,7 +2,12 @@ const { merge } = require('webpack-merge');
 const commonConfiguration = require('./webpack.common.js');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-module.exports = merge(commonConfiguration, {
-    mode: 'production',
-    plugins: [new CleanWebpackPlugin()],
-});
+module.exports = merge(
+    commonConfiguration,
+    {
+        mode: 'production',
+        plugins: [
+            new CleanWebpackPlugin(),
+        ],
+    }
+);
